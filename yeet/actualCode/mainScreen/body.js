b98a8b0733a75bab7cf1af7509b48308c25d4476
loravocado <lora@ualberta.ca>
2020-01-18 22:25:43 -0700

    Did most of the first two screen but ran into a bug that made me extremely sad

--- yeet/actualCode/mainScreen/body.js
+++ yeet/actualCode/mainScreen/body.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Image, Button, Text, Modal} from 'react-native';
+import InputName from '../inputName/inputNameScreen.js';
 
-export default class header extends Component {
+export default class body extends Component {
   render() {
     return (
       <View style={main_body.container}>
-        <View style={main_body.ghost_image_wrapper}>
+        <View style={main_body.borderContainer}>
           <Image
-           style={main_body.ghost_image}
-           source={require('../../images/blue_ghost.png')}
+           style={main_body.border}
+           source={require('../../images/border.png')}
+          />
+        </View>
+        <View style={main_body.main_body}>
+          <View style={main_body.ghost_image_wrapper}>
+            <Image
+             style={main_body.ghost_image}
+             source={require('../../images/blue_ghost.png')}
+            />
+          </View>
+        <InputName/>
+        </View>
+        <View style={main_body.borderContainer}>
+          <Image
+           style={main_body.border}
+           source={require('../../images/border.png')}
           />
         </View>
       </View>
@@ -19,20 +35,30 @@ export default class header extends Component {
 const main_body = StyleSheet.create({
   container: {
     flex:10,
-    backgroundColor:'#282626'
+    backgroundColor:'#282626',
+    flexDirection:'row'
   },
   ghost_image_wrapper: {
     alignItems:'center',
     flex:10,
     justifyContent: 'center',
-    marginBottom:100,
+    marginBottom:50,
   },
   ghost_image: {
     width:240,
     height:240
   },
-  button_wrapper: {
+  borderContainer: {
     flex:1
   },
+  border: {
+    height:705,
+    width: 9,
+    margin:5,
 
+  },
+
+  main_body: {
+    flex:20
+  },
 })
