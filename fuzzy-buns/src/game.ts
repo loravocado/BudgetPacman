e4ba842c83a4dc6035987f53390417549c3a96d6
Mackenzie Malainey <macmalainey@gmail.com>
2020-01-19 08:53:43 -0700

    We can now only hope...

--- fuzzy-buns/src/game.ts
+++ fuzzy-buns/src/game.ts
@@ -37,6 +37,9 @@ class GameMaster {
   pellets: Array<Pellet>;
 }
 
+let map_x : number;
+let map_y : number;
+
 let game: GameMaster = null;
 
 export function init_game_state() {
@@ -142,15 +145,45 @@ function register_user(info) {
 }
 
 function generate_pts(lat: number, lng: number) {
+  map_x = lat;
+  map_y = lng;
   console.log("Generating Pellets");
-  const spawn = require("child_process").spawn;
-  const pythonProcess = spawn("python", ["roads/start.py", lat, lng]);
+  var express = require('express'); 
+  var app = express(); 
 
-  let ptId = 0;
+  // Creates a server which runs on port 3000 and  
+  // can be accessed through localhost:3000 
+  app.listen(3000, function() { 
+      console.log('server running on port 3000'); 
+  } ) 
+    
+  // Function callName() is executed whenever  
+  // url is of the form localhost:3000/name 
+  app.get('/name', callName); 
+}
 
-  pythonProcess.stdout.on("data", data => {
+function callName(req, res) { 
+        
+  // Use child_process.spawn method from  
+  // child_process module and assign it 
+  // to variable spawn 
+  var spawn = require("child_process").spawn; 
+    
+  // Parameters passed in spawn - 
+  // 1. type_of_script 
+  // 2. list containing Path of the script 
+  //    and arguments for the script  
+    
+  // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will 
+  // so, first name = Mike and last name = Will 
+  var process = spawn('python', ["roads/start.py", map_x, map_y]); 
+
+  // Takes stdout data from script which executed 
+  // with arguments and send this data to res object 
+  process.stdout.on('data', data => {
     console.log("Data returned");
     let splitData = data.split("/\r?\n/");
+    let ptId = 0;
     splitData.forEach(element => {
       let cords = splitData.split(",");
       let pel = new Pellet();
@@ -162,4 +195,4 @@ function generate_pts(lat: number, lng: number) {
       console.log([pel.lat, pel.lng]);
     });
   });
-}
+} 
