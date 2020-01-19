36564ba69dcb56076f76a382b7d51df6a8fdf3c1
Mackenzie Malainey <macmalainey@gmail.com>
2020-01-19 04:39:03 -0700

    C# > Node

--- fuzzy-buns/src/game.ts
+++ fuzzy-buns/src/game.ts
@@ -5,7 +5,6 @@ export class Player {
     name : string;
     lat : number;
     lng : number;
-    timestamp : Date;
 }
 
 enum States {
@@ -18,6 +17,7 @@ enum States {
 class GameMaster {
     state : States;
     players : Array<Player>;
+    main : Pacman;
     pellets : Array<Pellet>;
 }
 
@@ -31,16 +31,86 @@ function init_game_state() {
     }
 }
 
-function process_user_state() {
+function process_user_update(info) {
 
+    let player : Player = null;
+    game.players.forEach(element => {
+        if (info.deviceId == element.UUID) {
+            player = element;
+            return;
+        }
+    });
+
+    if (player == null && info.type == "PLAYER_STATE") {
+        register_user(info)
+    } else {
+        if (info.type == "PLAYER_STATE") {
+            player.lat = info.lat;
+            player.lng = info.lng;
+            if (player.UUID == game.main.UUID && (game.state == States.Hide || game.state == States.Chase)) {
+                info.eatenPellets.forEach(element => {
+                    let pel = game.pellets.find(element2 => element2.id == element.id);
+                    game.pellets.splice(game.pellets.indexOf(pel), 1);
+                });
+                if (game.pellets.length == 0) {
+                    return States.GameOver;
+                }
+            } else if (game.state == States.Chase && compareCords(player.lat, player.lng, game.main.lat, game.main.lng)) {
+                game.main.lives--;
+                if (game.main.lives == 0) {
+                    return States.GameOver;
+                }
+            }
+        } else if (info.type == "GAME_STATE" && game.state == States.Lobby && game.players.length > 1) {
+            return States.Chase;
+        }
+    }
+
+    return game.state;
 }
 
-function register_user(UUID : string, name : string) {
+function compareCords(lat1 : number, lng1 : number, lat2 : number, lng2 : number) {
+    if (Math.abs(lat1 - lat2) < 0.0001 && Math.abs(lng1 - lng2) < 0.0001) {
+        return true;
+    }
+    return false;
+}
+
+function register_user(info) {
     if (game.state == States.Lobby) {
-        
+        let create_pellets : boolean = false;
+        if (game.players.length == 0) {
+            create_pellets = true;
+        }
+        let noob : Player = new Player();
+        noob.UUID = info.deviceId;
+        noob.lat = info.lat;
+        noob.lng = info.lng;
+        noob.name = info.name;
+        game.players.concat(noob);
+
+        if (create_pellets) {
+            generate_pts(info.lat, info.lng);
+        }
     }
 }
 
 function generate_pts(lat : number, lng : number) {
+    const spawn = require("child_process").spawn;
+    const pythonProcess = spawn('python', ["roads/start.py", lat, lng]);
 
+    let ptId = 0;
+
+    pythonProcess.stdout.on('data', (data) => {
+        let splitData = data.split('/\r?\n/');
+        splitData.forEach(element => {
+            let cords = splitData.split(',');
+            let pel = new Pellet();
+            pel.id = ptId;
+            pel.lat = cords[0];
+            pel.lng = cords[1];
+            ptId++;
+            game.pellets.concat(pel);
+        });
+    });
 }
