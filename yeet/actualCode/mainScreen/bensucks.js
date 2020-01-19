ac8d8a270d6a234dcc1490437685333444ff59ba
AtrazineC <benjaminkong17@gmail.com>
2020-01-19 02:09:32 -0700

    road snapping lmao

--- yeet/actualCode/mainScreen/bensucks.js
+++ yeet/actualCode/mainScreen/bensucks.js
@@ -1,12 +1,358 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
+import {Button, StyleSheet, View, Platform, Dimensions} from "react-native";
+import MapView, {Marker, AnimatedRegion, PROVIDER_GOOGLE} from 'react-native-maps';
+
+const {width, height} = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.002;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+var apiKey = 'AIzaSyDVfVr11MvcKgNNlW6TSRwX2a3VhTzs4k8';
 
 export default class header extends Component {
+  mapStyle = [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#212121"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.country",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.locality",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "weight": 1.5
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#181818"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "weight": 0.5
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#1b1b1b"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#2c2c2c"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#8a8a8a"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#373737"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#3c3c3c"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway.controlled_access",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#4e4e4e"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#000000"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#3d3d3d"
+        }
+      ]
+    }
+  ]
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      coordinate: new AnimatedRegion({
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      })
+    };
+  }
+
+  UpdateLocation = () => {
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        const { coordinate } = this.state;
+        const { latitude, longitude } = position.coords;
+
+        var newCoordinate = {
+          latitude,
+          longitude
+        };
+
+        {(async () => {
+          console.log('---------------------------------------------')
+          console.log(latitude + ', ' + longitude)
+
+          await fetch('https://roads.googleapis.com/v1/snapToRoads?path=' + latitude + ',' + longitude + '&key=AIzaSyDVfVr11MvcKgNNlW6TSRwX2a3VhTzs4k8')
+          .then(response => response.json())
+          .then((responseJson)=> {
+            const a = responseJson.snappedPoints[0].location
+
+            this.setState({
+              latitude: a.latitude,
+              longitude: a.longitude
+            });
+
+            newCoordinate = {latitude: this.state.latitude, longitude: this.state.longitude}
+
+            if (Platform.OS === "android") {
+              if (this.marker) {
+                this.marker._component.animateMarkerToCoordinate(
+                  newCoordinate,
+                  500
+                );
+              }
+            } else {
+              coordinate.timing(newCoordinate).start();
+            }
+
+            console.log(this.state.latitude + ', ' + this.state.longitude)
+          })
+          .catch(error => console.log('Too far from road')) //to catch the errors if any
+        })();}
+        
+      },
+      error => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }
+
+  componentDidMount() {
+    this.UpdateLocation()
+  }
+
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  });
+
   render() {
     return (
-      <View style={{flex:1, backgroundColor:'#282626', position:'absolute'}}>
-        <Text> Hello </Text>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          showUserLocation
+          followUserLocation
+          loadingEnabled
+          region={this.getMapRegion()}
+          customMapStyle={this.mapStyle}
+        >
+          <Marker.Animated
+            ref={marker => {
+              this.marker = marker;
+            }}
+            coordinate={this.state.coordinate}
+            title={'Lora'}
+            description={'Sucks'}
+          />
+        </MapView>
+
+        <Button
+          title='Update Location'
+          onPress={async () => {
+            this.UpdateLocation()
+          }}
+        >
+        </Button>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: 70
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom:0,
+    right: 0
+  }
+});
