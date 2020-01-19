047420109b93117156e2eb26efd05089431e8765
Michael Kwok <michael.kwok00@gmail.com>
2020-01-19 00:54:58 -0700

    send stufF?

--- yeet/actualCode/backend/ServerConnection.js
+++ yeet/actualCode/backend/ServerConnection.js
@@ -1,14 +1,14 @@
 import io from "socket.io-client";
-import gameState from "GameState";
+import gameState from "./GameState.js";
 
 class ServerConnection {
     static instance = ServerConnection.instance || new ServerConnection();
-    static socket = io.connect('34.82.20.254')
+    socket = io.connect('34.82.20.254')
     constructor() {
     }
 
     Register() {
-        socket.emit("login");
+        this.socket.emit("login", gameState);
     }
 }
 
